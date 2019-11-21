0378355a36c2e67ce2109caa9afca2c50e47e98c
Tomas Gallucci <professortom@gmail.com>
2019-11-21 08:52:50 -0600

    move file logic works
    debating on where to put the time stamp that becomes
    the name of the archive directory

--- brook_python/brook_python.py
+++ brook_python/brook_python.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
+import os
+from datetime import datetime
+from pathlib import Path
+
+DB_NAME = 'some.db'
+CUSTOMER_CSV = 'CUSTOMER.CSV'  # UPPERCASE come from assignment
+INVOICE_CSV = 'INVOICE.CSV'  # UPPERCASE come from assignment
+INVOICE_ITEM_CSV = 'INVOICE_ITEM.CSV'  # UPPERCASE come from assignment
+CUSTOMER_SAMPLE_CSV = 'CUSTOMER_SAMPLE.CSV'  # UPPERCASE come from assignment
+
+
+def move_single_file(fileName, dateAndTimeStamp):
+    if os.path.isfile(fileName):
+        Path(fileName).rename(os.path.join(dateAndTimeStamp, fileName))
+
+
+def move_files():
+    curr_time = datetime.now()
+    dateAndTimeStamp = curr_time.strftime('%m-%d-%Y-%H-%M-%S-%f')
+
+    if not os.path.isdir(dateAndTimeStamp):
+        os.makedirs(dateAndTimeStamp)
+
+    move_single_file(CUSTOMER_CSV, dateAndTimeStamp)
+    move_single_file(INVOICE_CSV, dateAndTimeStamp)
+    move_single_file(INVOICE_ITEM_CSV, dateAndTimeStamp)
+    move_single_file(CUSTOMER_SAMPLE_CSV, dateAndTimeStamp)  
+
+
 if __name__ == "__main__":
-    pass
+    move_files()
